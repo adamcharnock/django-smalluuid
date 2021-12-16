6b7ce3d7c18fdb58ed374cb203e6f9695ec281c6
Petr Dlouhý <petr.dlouhy@email.cz>
2021-12-16 12:44:39 +0100

    fixes for Django 4.0

--- django_smalluuid/models.py
+++ django_smalluuid/models.py
@@ -6,7 +6,7 @@ from django.db.models.fields import UUIDField
 import six
 from django.utils.deconstruct import deconstructible
 from django.utils.module_loading import import_string
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 import smalluuid
 
 from django_smalluuid import settings, forms
